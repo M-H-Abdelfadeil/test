c67128f95a3de7a8f91b6da56aecd0cc1ad086ba
Mahmoud Abdelfadiel <60010586+M-H-Abdelfadeil@users.noreply.github.com>
2022-03-18 00:07:55 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+var app = document.getElementById('app');
+app.innerHTML
+
+
+
+
+=`
+
+
+
+
+<div class="test-class" id="text">test</div>
+<img src='dd' id="icon">
+
+
+<br><br>
+
+<form>
+    <input name="name" placeholder="name"><br><br>
+    <input name="phone" placeholder="phone"><br><br>
+    <input name="address" placeholder="address"><br><br>
+    <input name="quantity" placeholder="quantity"><br><br>
+    <input type="submit">
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+`
