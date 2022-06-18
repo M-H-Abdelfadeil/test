37596d8d444ab97110764f0c59ab93adac0c8c0a
Mahmoud Abdelfadiel <60010586+M-H-Abdelfadeil@users.noreply.github.com>
2022-06-18 02:54:53 +0200

    Update and rename test.php to test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+var app = document.getElementById('app');
+app.innerHTML
+
+
+
+
+=`
+<div class="test-class" id="text"></div>
+<img src='' id="icon">
+<br><br>
+<form>
+    <input name="name" placeholder="name"><br><br>
+    <input name="phone" placeholder="phone"><br><br>
+    <input name="address" placeholder="address"><br><br>
+    <input name="quantity" placeholder="quantity"><br><br>
+    <input type="submit">
+</form>
